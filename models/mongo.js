12a5ad2d9abd6c1d98f572fb5313b3636df9e5cb
Paolo Cozzi <bunop@libero.it>
2020-02-14 14:51:58 +0100

    :bug: retry connection if necessary

--- models/mongo.js
+++ models/mongo.js
@@ -1,5 +1,17 @@
-var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://mongo:27017/demoDb');
+var mongoose = require("mongoose");
+var mongoUrl = 'mongodb://mongo:27017/demoDb'
+
+// https://gist.github.com/asalant/4092454
+var connectWithRetry = function() {
+  return mongoose.connect(mongoUrl, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
+
 var mongoSchema =   mongoose.Schema;
 var userSchema  = {
     "userEmail" : String,
